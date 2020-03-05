2a6b8d3cbf249fa8050b66c1efb0bb4bc571620a
Barry Anderson <barryanderson@users.noreply.github.com>
2020-03-05 14:07:38 +0000

    Updated some chat logic including link and image processing.

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
+const chalk = require('chalk');
 
 const port = process.env.PORT || '3000';
+
+let users = [];
+const userColors = ['#C269FE', '#63E9FC', '#FFC48E', '#DDB9B9', '#FFF7B7'];
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
@@ -13,21 +18,29 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
 
-  io.emit('user-joins', 'A new user connected.');
-
   socket.on('disconnect', () => {
-    io.emit('user-leaves', 'A user disconnected.');
+    if(socket.username) {
+      users.splice(users.indexOf(socket.username.toLowerCase()), 1);
+      io.emit('user-leaves', socket.username, Date.now());
+    }
+  });
+
+  socket.on('username-choice', (username) => {
+    if(!users.includes(username.toLowerCase())) {
+      users.push(username.toLowerCase());
+      socket.username = username;
+      socket.emit('username-ok');
+      io.emit('user-joins', socket.username, Date.now());
+    } else {
+      socket.emit('username-taken', username);
+    }
   });
 
   socket.on('chat-message', function(msg){
-    if(msg.startsWith('img:')) {
-      io.emit('chat-message', msg.replace('img:', ''), Date.now(), socket.id, true);
-    } else {
-      io.emit('chat-message', msg, Date.now(), socket.id);
-    }
+    io.emit('chat-message', msg, Date.now(), socket.id, socket.username, userColors[users.indexOf(socket.username.toLowerCase())]);
   });
 });
 
 http.listen(port, () => {
-  console.log(`Listening on  *:${port}`);
+  console.log(chalk.blue(`Listening on *:${port}`));
 })
--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,9 +1,47 @@
 const socket = io();
 
+// Regex match strings.
+const urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+const imgRegex = /(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:jpg|jpeg|gif|png))(?:\?([^#]*))?(?:#(.*))?/gi;
+
+// Capture required dom elements.
 const formEl = document.getElementById('message-form');
 const messageInputEl = document.getElementById('message-input');
 const messageListEl = document.getElementById('messages');
 
+// Title and unread messages.
+const title = 'UoD Chat Demo';
+let unreadMessages = 0;
+
+// Capture username to before starting.
+captureUsername();
+
+// Get username.
+function captureUsername() {
+  let usernameCaptured = false;
+  while(!usernameCaptured) {
+    let username = '';
+    username = prompt('Please provide a username', '');
+    username = username.trim();
+    if(!username || username == "") {
+      usernameCaptured = false;
+    } else {
+      usernameCaptured = true;
+      socket.emit('username-choice', username);
+    }
+  }
+}
+
+socket.on('username-ok', () => {
+  document.body.style.display = "block";
+});
+
+socket.on('username-taken', (username) => {
+  alert(`Username ${username} is already taken.\n\nPlease try again.\n\n`);
+  captureUsername();
+});
+
+// Listen for form submissions.
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();  // Prevent page reloading.
   socket.emit('chat-message', messageInputEl.value);
@@ -11,43 +49,105 @@ formEl.addEventListener('submit', (e) => {
   return false;
 });
 
-socket.on('chat-message', function(message, timestamp, socketid, isImg){
+// Handle chat message events.
+socket.on('chat-message', (message, timestamp, socketid, username, usercolor) => {
   // Create timestamp for message.
-  let timestampEl = document.createElement('span');
+  const timestampEl = document.createElement('span');
   timestampEl.innerHTML = `[${formatDate(timestamp)}] `
   timestampEl.style.fontWeight = 'bold';
 
-  // Create message with timestamp.
-  let messageItemEl = document.createElement('li');
-  messageItemEl.appendChild(timestampEl);
+  // Create username for message.
+  const nameEl = document.createElement('span');
+  nameEl.innerHTML = (socket.id === socketid) ? 'You: ' : `${username}: `;
+  nameEl.style.fontWeight = 'bold';
 
-  // Playing about with adding images ... messages prefixed with "img:" fall into this.
-  if(isImg) {
-    let linkEl = document.createElement('a');
+  // Create message with timestamp.
+  const messageItemEl = document.createElement('li');
+  messageItemEl.appendChild(timestampEl);
+  messageItemEl.appendChild(nameEl);
+
+  // Playing about with parsing images and links.
+  const isUrl = urlRegex.test(message);
+  const isImg = imgRegex.test(message);
+  // console.log(`IsUrl: ${isUrl}, IsImg: ${isImg}`);
+  if(isUrl || isImg) {
+    const linkEl = document.createElement('a');
     linkEl.href = message;
     linkEl.target = '_blank';
 
-    let imgEl = document.createElement('img');
-    imgEl.src = message;
-    imgEl.width = 200;
-    imgEl.style.paddingTop = '5px'
+    if(isImg) {
+      const imgEl = document.createElement('img');
+      imgEl.src = message;
+      imgEl.width = 200;
+      imgEl.style.paddingTop = '5px'
 
-    linkEl.appendChild(imgEl);
+      linkEl.appendChild(imgEl);
+      messageItemEl.append(document.createElement('br'));
+    } else {
+      linkEl.innerText = message;
+    }
 
-    messageItemEl.append(document.createElement('br'));
     messageItemEl.append(linkEl);
   } else {
     messageItemEl.append(message);
   }
 
   // If this is the sending client mark as own message for styling.
-  if(socket.id === socketid) messageItemEl.className = 'own';
+  if(socket.id === socketid) {
+    messageItemEl.className = 'own'
+  } else {
+    messageItemEl.style.backgroundColor = usercolor;
+  };
 
   // Append to list and scroll to bottom.
   messageListEl.appendChild(messageItemEl)
   messageListEl.scrollTop = messageListEl.scrollHeight;
+
+  unreadMessages++;
+  changeTitle();
 });
 
+socket.on('user-joins', (username, timestamp) => {
+  // Create timestamp for message.
+  const timestampEl = document.createElement('span');
+  timestampEl.innerHTML = `[${formatDate(timestamp)}] `
+  timestampEl.style.fontWeight = 'bold';
+
+  // Create message with timestamp.
+  const messageItemEl = document.createElement('li');
+  messageItemEl.appendChild(timestampEl);
+  messageItemEl.append(`${username} has joined.`);
+  messageItemEl.className = 'join';
+
+  // Append to list and scroll to bottom.
+  messageListEl.appendChild(messageItemEl)
+  messageListEl.scrollTop = messageListEl.scrollHeight;
+
+  unreadMessages++;
+  changeTitle();
+});
+
+socket.on('user-leaves', (username, timestamp) => {
+  // Create timestamp for message.
+  const timestampEl = document.createElement('span');
+  timestampEl.innerHTML = `[${formatDate(timestamp)}] `
+  timestampEl.style.fontWeight = 'bold';
+
+  // Create message with timestamp.
+  const messageItemEl = document.createElement('li');
+  messageItemEl.appendChild(timestampEl);
+  messageItemEl.append(`${username} has left.`);
+  messageItemEl.className = 'leave';
+
+  // Append to list and scroll to bottom.
+  messageListEl.appendChild(messageItemEl)
+  messageListEl.scrollTop = messageListEl.scrollHeight;
+
+  unreadMessages++;
+  changeTitle();
+});
+
+// Make a datetime stamp prettier.
 function formatDate(timestamp) {
   const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   const date = new Date(timestamp);
@@ -60,3 +160,18 @@ function formatDate(timestamp) {
   // return `${day}-${month}-${year} ${hour}:${mins}:${secs}`
   return `${hour}:${mins}:${secs}`
 }
+
+// Change page title to show unread messages.
+function changeTitle() {
+  if(unreadMessages > 0 && !document.hasFocus()) {
+    document.title = `(${unreadMessages}) ${title}`;
+  } else {
+    document.title = `${title}`;
+    unreadMessages = 0;  // Page has focus zero the count of unread.
+  }
+}
+
+// Change title when window is focused on, ensuring unread title is zeroed.
+window.addEventListener('focus', (e) => {
+  changeTitle();
+});
